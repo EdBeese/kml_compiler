339df863e9409a3611a209ef6b8004c1eba18b77
Edward Beesley <edwardjbeesley@gmail.com>
2023-02-17 16:44:17 +0000

    set the run program to create a JSON of the extractor response

--- lib/run.rb
+++ lib/run.rb
@@ -1,6 +1,9 @@
 require_relative 'kml_coordinate_extractor'
+require 'json'
 require 'pry'
 
 response = KmlCoordinatesExtractor.file_iterator('kmls')
+
+File.open('output.json', 'w') { |f| f.write(response.to_json) }
 
 # Write further code to manipulate response
